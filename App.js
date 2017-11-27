e0bd20d3c1181df79ffd7f73389a12358b2fa757
Cooper <cooper.halvordson@uconn.edu>
2017-11-27 15:05:48 -0500

    Note screen can get text input from keyboard, product screen has two products with a name, picture, rating for each. I attempted to implement stacknavigator in App, but it breaks the Note screen.

--- App.js
+++ App.js
@@ -1,53 +1,15 @@
-import React from 'react';
-import { Alert, Button, Image, StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { AppRegistry, Alert, Button, Image, StyleSheet, Text, View, TextInput, Keyboard } from 'react-native';
 import { StackNavigator } from 'react-navigation';
-var page=1;
+import noteScreen from './noteScreen';
+import productScreen from './productScreen';
 
-export default class App extends React.Component {
-	nextPage() {
-		page=page+1;
-	}
-	previousPage() {
-		page=page-1;
-	}
-  render() {
-	while(page==1) {
-	return (
-	<View style={styles.cover}>
-		<Text>Passport</Text>
-		<View style={styles.button}>
-			<Button
-				title="Next Page"
-				onPress= {nextPage()}
-			/>
-		</View>
-	</View>
-	);
-	}
-  
-	while(page==2) {
-	return (
-	<View style={styles.book}>
-		<View style={styles.row}>
-			<Image source={require('./assets/download.jpg')} />
-			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
-		</View>
-		<View style={styles.row}>
-			<Image source={require('./assets/download2.jpg')} />
-			<Text style={{flex: 1, flexWrap: 'wrap'}}>Joe's Winery: Joe's Winery has the best Port in CT!</Text>
-		</View>
-		<Text>This is a very basic skeleton of the passport, minus formatting and stamp functionality </Text>
-		<View style={styles.button}>
-			<Button
-				onPress= {previousPage()}
-				title="Previous Page"
-			/>
-		</View>
-	</View>
-	);
-	}
-  }
-}
+const App = StackNavigator({
+	Note: { screen: noteScreen},
+	Product: { screen: productScreen},
+});
+
+export default App;
 const styles = StyleSheet.create({
   cover: {
     flex: 1,
