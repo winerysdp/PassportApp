98337efd4647c924be73603164ea0c3c0b456067
nathanmilne <nathan.milne@uconn.edu>
2018-02-05 10:56:49 -0500

    Add files via upload

--- PassNav.js
+++ PassNav.js
@@ -13,8 +13,394 @@ export default class passNav extends Component {
 		const { navigate } = this.props.navigation;
 	return (
 	<View style={styles.book}>
+		<ScrollView contentContainerStyle={styles.contentContainer}>
 		<Text style={{fontSize: 30}}> My Passport </Text>
 		<Text style={{fontSize: 14, textAlign: 'center'}}> Click on images below to navigate to corresponding Winery pages </Text>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
+		<View style={styles.row}>
+			<Image 
+				source={require('./assets/download.jpg')}
+				style={styles.navImage}
+			/>
+			<Text style={{flex: 1, flexWrap: 'wrap'}}>Bob's Winery: Bob's Winery is a nice place to eat.</Text>
+			<Image 
+				source={require('./assets/no-stamp.png')}
+				style={styles.navImage}
+			/>
+		</View>
 		<View style={styles.row}>
 			<Image 
 				source={require('./assets/download.jpg')}
@@ -43,11 +429,15 @@ export default class passNav extends Component {
 				onPress= {() => navigate('Winery')}
 			/>
 		</View>
+		</ScrollView>
 	</View>
 	);
   }
 }
 const styles = StyleSheet.create({
+  contentContainer: {
+	paddingVertical: 20
+  },
   book: {
     flex: 1,
     backgroundColor: '#ffebcd',
